b78a392fd1dc48e3306cf8096ab4c85dae4a2923
MateuszKacperski <kacperskimateusz95@gmail.com>
2023-12-22 16:12:03 +0100

    added tuttifrutti

--- array-lista/js/script.js
+++ array-lista/js/script.js
@@ -1,1 +1,12 @@
 console.log('Js ok');
+
+
+//Creo arry frigo
+const frigo = ['banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino', 'cocomero', 'limone', 'fragola'];
+//Creo variabile frutto
+const frutto = 'pesca';
+
+//Aggiungo la variabile all'array
+frigo.push(frutto);
+//Stampo in console
+console.table(frigo);
